8b612b64e79790bb5a9d9b9b7afd0172e3e2911c
Marcel Weisgut <marcel-weisgut@web.de>
2020-08-11 15:00:08 +0200

    disabled _scan_sorted_segment

--- src/lib/operators/table_scan/column_vs_value_table_scan_impl.cpp
+++ src/lib/operators/table_scan/column_vs_value_table_scan_impl.cpp
@@ -33,17 +33,17 @@ std::string ColumnVsValueTableScanImpl::description() const { return "ColumnVsVa
 void ColumnVsValueTableScanImpl::_scan_non_reference_segment(
     const AbstractSegment& segment, const ChunkID chunk_id, RowIDPosList& matches,
     const std::shared_ptr<const AbstractPosList>& position_filter) {
-  const auto& chunk_sorted_by = _in_table->get_chunk(chunk_id)->sorted_by();
+  // const auto& chunk_sorted_by = _in_table->get_chunk(chunk_id)->sorted_by();
 
-  if (!chunk_sorted_by.empty()) {
-    for (const auto& sorted_by : chunk_sorted_by) {
-      if (sorted_by.column == _column_id) {
-        _scan_sorted_segment(segment, chunk_id, matches, position_filter, sorted_by.sort_mode);
-        ++chunk_scans_sorted;
-        return;
-      }
-    }
-  }
+  // if (!chunk_sorted_by.empty()) {
+  //   for (const auto& sorted_by : chunk_sorted_by) {
+  //     if (sorted_by.column == _column_id) {
+  //       _scan_sorted_segment(segment, chunk_id, matches, position_filter, sorted_by.sort_mode);
+  //       ++chunk_scans_sorted;
+  //       return;
+  //     }
+  //   }
+  // }
 
   if (const auto* dictionary_segment = dynamic_cast<const BaseDictionarySegment*>(&segment)) {
     _scan_dictionary_segment(*dictionary_segment, chunk_id, matches, position_filter);
