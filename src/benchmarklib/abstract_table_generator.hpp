10649311431eeabf0388403990b06a55f5fcbf30
Marcel Weisgut <marcel-weisgut@web.de>
2020-08-11 11:46:10 +0200

    lineitem chunk size 10,000

--- src/benchmarklib/abstract_table_generator.hpp
+++ src/benchmarklib/abstract_table_generator.hpp
@@ -63,7 +63,7 @@ class AbstractTableGenerator {
 
   static std::shared_ptr<BenchmarkConfig> create_benchmark_config_with_chunk_size(ChunkOffset chunk_size);
 
-  constexpr static uint32_t lineitem_chunk_size = 30000u;
+  constexpr static uint32_t lineitem_chunk_size = 10000u;
 
  protected:
   // Creates indexes, expects the table to have been added to the StorageManager and, if requested, encoded
